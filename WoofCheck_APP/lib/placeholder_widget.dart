e054de1101a26de6f2c92ef508c24bf553dc7184
Rahul Dani <32138368+15dani1@users.noreply.github.com>
2020-02-12 19:10:52 -0500

    Firebase linked

--- WoofCheck_APP/lib/placeholder_widget.dart
+++ WoofCheck_APP/lib/placeholder_widget.dart
@@ -1,14 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class PlaceholderWidget extends StatelessWidget {
- final Color color;
+  final Color color;
+  final databaseReference = Firestore.instance;
+  PlaceholderWidget(this.color);
 
- PlaceholderWidget(this.color);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: RaisedButton(
+        onPressed: () {
+          createRecord('JOhn Cena');
+        },
+        color: Colors.blue,
+        splashColor: Colors.red,
+      ),
+      color: color,
+    );
+  }
 
- @override
- Widget build(BuildContext context) {
-   return Container(
-     color: color,
-   );
- }
+  void createRecord(String yeet) async {
+    await databaseReference.collection("books").document("1").setData({
+      'title': yeet,
+      'description': 'Programming Guide for Dart'
+    });
+
+    DocumentReference ref = await databaseReference.collection("books").add({
+      'title': 'Flutter in Action',
+      'description': 'Complete Programming Guide to learn Flutter'
+    });
+    print(ref.documentID);
+  }
 }
